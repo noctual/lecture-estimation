0d3de9920edf2d49307512c23a65248ef46c7e62
ivans592@yandex.ru <60885752+noctual@users.noreply.github.com>
2021-05-06 12:16:43 +0500

    Create lecture list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import ReactionsList from './ReactionsList';
 import firebase from './Firebase';
 import Lecture from './Lecture';
+import LectureList from './LectureList';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <main className="main">
       <Switch>
+        <Route exact path='/' component={LectureList}/>
         <Route path='/lecture/:lectureId' component={Lecture}/>
       </Switch>
     </main>
---
+++ src/LectureList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import firebase from './Firebase';
+
+class LectureList extends Component{
+    constructor(props) {
+      super(props);
+      this.state = {lectures: []};
+    }
+  
+    componentDidMount(){
+      var estimationsRef = firebase.database().ref("/lectures");
+      estimationsRef.once('value', (snapshot) => {
+        let lectures = [];
+        snapshot.forEach(function(childSnapshot) {
+          lectures.push({key: childSnapshot.key, name: childSnapshot.val().name})
+        });
+        this.setState({lectures: lectures});
+      });
+    }
+  
+    render() {
+      const listItems = this.state.lectures.map(
+        (lec) => <li className="lecture-list__item"><h2><Link to={'/lecture/' + lec.key}>{lec.name}</Link></h2></li>
+      );
+      return (
+        <div className="lecture-list">
+          <h1 className="lecture-list__title">Лекции</h1>
+          <ul className="lecture-list__items">{listItems}</ul>
+        </div>
+      );
+    }
+}
+
+export default LectureList;
